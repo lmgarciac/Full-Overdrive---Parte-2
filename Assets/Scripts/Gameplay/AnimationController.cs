be4aa0950a793e3517bc7255e5bf4fef25c5d03f
Luis Garcia <luism.garciac@gmail.com>
2020-02-26 21:49:15 -0300

    Cambios Animator
    Cambios Animator

--- Assets/Scripts/Gameplay/AnimationController.cs
+++ Assets/Scripts/Gameplay/AnimationController.cs
@@ -6,21 +6,26 @@ using Events;
 public class AnimationController : MonoBehaviour
 {
 
-    [SerializeField] private GameObject player;
-    [SerializeField] private GameObject enemy;
-    [SerializeField] private GameObject camera;
+    //[SerializeField] private GameObject player;
+    //[SerializeField] private GameObject enemy;
+    //[SerializeField] private GameObject camera;
 
-    [SerializeField] private GameObject objplayerattack;
-    [SerializeField] private GameObject objplayeridle;
-    [SerializeField] private GameObject objplayerspecial;
+    //[SerializeField] private GameObject objplayerattack;
+    //[SerializeField] private GameObject objplayeridle;
+    //[SerializeField] private GameObject objplayerspecial;
 
-    [SerializeField] private GameObject objenemyattack;
-    [SerializeField] private GameObject objenemyidle;
-    [SerializeField] private GameObject objenemyspecial;
+    //[SerializeField] private GameObject objenemyattack;
+    //[SerializeField] private GameObject objenemyidle;
+    //[SerializeField] private GameObject objenemyspecial;
 
-    private Animator playeranim;
-    private Animator enemyanim;
-    private Animator cameraanim;
+
+    [SerializeField] private GameObject go_player;
+    private Animator an_player;
+
+
+    //private Animator playeranim;
+    //private Animator enemyanim;
+    //private Animator cameraanim;
 
     public enum animation
     {
@@ -42,9 +47,10 @@ public class AnimationController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playeranim = player.GetComponent<Animator>();
-        enemyanim = enemy.GetComponent<Animator>();
-        cameraanim = camera.GetComponent<Animator>();
+        //playeranim = player.GetComponent<Animator>();
+        //enemyanim = enemy.GetComponent<Animator>();
+        //cameraanim = camera.GetComponent<Animator>();
+        an_player = go_player.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -59,23 +65,32 @@ public class AnimationController : MonoBehaviour
             if (anim.animation == (int)animation.play)
             {
                 //playeranim.runtimeAnimatorController = Resources.Load("ANI_Main_Atack") as RuntimeAnimatorController;
-                objplayeridle.SetActive(false);
-                objplayerspecial.SetActive(false);
-                objplayerattack.SetActive(true);
+                //objplayeridle.SetActive(false);
+                //objplayerspecial.SetActive(false);
+                //objplayerattack.SetActive(true);
+                an_player.SetBool("Idle", false);
+                an_player.SetBool("Attack", true);
+                an_player.SetBool("Special", false);
             }
             else if (anim.animation == (int)animation.special)
             {
                 //playeranim.runtimeAnimatorController = Resources.Load("ANI_Main_Special") as RuntimeAnimatorController;
-                objplayeridle.SetActive(false);
-                objplayerspecial.SetActive(true);
-                objplayerattack.SetActive(false);
+                //objplayeridle.SetActive(false);
+                //objplayerspecial.SetActive(true);
+                //objplayerattack.SetActive(false);
+                an_player.SetBool("Idle", false);
+                an_player.SetBool("Attack", false);
+                an_player.SetBool("Special", true);
             }
             else //Idle
             {
                 //playeranim.runtimeAnimatorController = Resources.Load("ANI_Main_idle") as RuntimeAnimatorController;
-                objplayeridle.SetActive(true);
-                objplayerspecial.SetActive(false);
-                objplayerattack.SetActive(false);
+                //objplayeridle.SetActive(true);
+                //objplayerspecial.SetActive(false);
+                //objplayerattack.SetActive(false);
+                an_player.SetBool("Idle", true);
+                an_player.SetBool("Attack", false);
+                an_player.SetBool("Special", false);
             }
         }
         else //Enemy
@@ -83,23 +98,23 @@ public class AnimationController : MonoBehaviour
             if (anim.animation == (int)animation.play)
             {
                 //enemyanim.runtimeAnimatorController = Resources.Load("ANI_Enemy_Atack") as RuntimeAnimatorController;
-                objenemyidle.SetActive(false);
-                objenemyspecial.SetActive(false);
-                objenemyattack.SetActive(true);
+                //objenemyidle.SetActive(false);
+                //objenemyspecial.SetActive(false);
+                //objenemyattack.SetActive(true);
             }
             else if (anim.animation == (int)animation.special)
             {
                 //enemyanim.runtimeAnimatorController = Resources.Load("ANI_Enemy_Special") as RuntimeAnimatorController;
-                objenemyidle.SetActive(false);
-                objenemyspecial.SetActive(true);
-                objenemyattack.SetActive(false);
+                //objenemyidle.SetActive(false);
+                //objenemyspecial.SetActive(true);
+                //objenemyattack.SetActive(false);
             }
             else
             {
                 //enemyanim.runtimeAnimatorController = Resources.Load("ANI_Enemy_idle") as RuntimeAnimatorController;
-                objenemyidle.SetActive(true);
-                objenemyspecial.SetActive(false);
-                objenemyattack.SetActive(false);
+                //objenemyidle.SetActive(true);
+                //objenemyspecial.SetActive(false);
+                //objenemyattack.SetActive(false);
             }
         }
 
