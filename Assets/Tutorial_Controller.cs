80350be8971b497f7abd2ba0f30c865b93c8426a
Luis Garcia <luism.garciac@gmail.com>
2020-03-21 20:50:58 -0300

    Implementación Comic Inicial
    Implementación Comic Inicial

--- Assets/Tutorial_Controller.cs
+++ Assets/Tutorial_Controller.cs
@@ -136,7 +136,7 @@ public class Tutorial_Controller : MonoBehaviour
         if (tutorialphase == 3 && !dialoguePlayer.isPlaying && startdialogue == false)
         {
             StartCoroutine(FadeTime(1f, fadeDurationinitial));
-            sceneController.FadeAndLoadScene("_Test_Navigation");
+            sceneController.FadeAndLoadScene("_Scene_Comic");
         }
 
     }
