99cc2a1d2ecc3dd902f15638056d4048a96d7930
Geropaxd <geroherna@gmail.com>
2020-06-04 16:15:05 -0300

    Nuevos semáforos en zona preentrega

---
+++ Assets/Scripts/Semáforos/ControlSemaforo.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class ControlSemaforo : MonoBehaviour
+{
+    public float _tiempoAbierto = 8f;
+    public float _tiempoAlerta = 3f;
+    public Semaforo[] _semaforos = new Semaforo[2];
+
+    private int _señalActual = 0;
+    private float _controlTiempo = 0;
+    private bool _emitirAlerta = false;
+
+    void Start()
+    {
+        AlterarSeñal();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_controlTiempo >= _tiempoAlerta && !_emitirAlerta)
+        {
+            _emitirAlerta = true;
+
+            _semaforos[_señalActual].AlertaSeñal();
+        }
+        else if (_controlTiempo >= _tiempoAbierto)
+        {
+            _emitirAlerta = false;
+            _controlTiempo = 0;
+            _semaforos[_señalActual].CerrarSeñal();
+            AlterarSeñal();
+        }
+
+        _controlTiempo += (1 * Time.deltaTime);
+    }
+
+    private void AlterarSeñal()
+    {
+        ProximaSeñal();
+        for (int i = 0; i<_semaforos.Length;i++)
+        {
+            if (i == _señalActual)
+            {
+                _semaforos[i].AbrirSeñal();
+            }
+            else
+            {
+                _semaforos[i].CerrarSeñal();
+            }
+        }
+    }
+
+    private void ProximaSeñal ()
+    {
+        if(_señalActual < 1)
+        {
+            _señalActual++;
+        }
+        else
+        {
+            _señalActual = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Semáforos/Semaforo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Semaforo : MonoBehaviour
+{
+    public GameObject SeñalVerde;
+    public GameObject SeñalAmarilla;
+    public GameObject SeñalRojo;
+    public Light Luz;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CerrarSeñal();
+    }
+
+    public void CerrarSeñal()
+    {
+        SeñalVerde.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+        SeñalAmarilla.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+        SeñalRojo.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+        Luz.color = Color.red;
+    }
+
+    public void AlertaSeñal()
+    {
+        SeñalVerde.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+        SeñalAmarilla.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+        SeñalRojo.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+        Luz.color = Color.yellow;
+    }
+
+    public void AbrirSeñal()
+    {
+        SeñalVerde.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+        SeñalAmarilla.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+        SeñalRojo.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+        Luz.color = Color.green;
+    }
+
+}
