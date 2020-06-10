58555dc87dfcac792053cd1b94dd464f981ef093
Luis Garcia <luism.garciac@gmail.com>
2020-06-09 22:50:23 -0300

    Correccion Palomas que Picotean
    Correccion Palomas que Picotean

---
+++ Assets/Pigeon_Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pigeon_Controller : MonoBehaviour
+{
+    private Animator animator;
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        animator.speed = Random.Range(0.7f, 1.8f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 2.0f)
+        {
+            animator.speed = Random.Range(0.7f, 1.8f);
+            time = 0f;
+        }
+    }
+}
